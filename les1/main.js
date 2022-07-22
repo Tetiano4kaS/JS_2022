b54d3426450585ae29d2178105ad6c4f3926442f
Tetiano4kaS <102378668+Tetiano4kaS@users.noreply.github.com>
2022-07-22 16:39:27 +0300

    add hw1

---
+++ les1/main.js
@@ -0,0 +1,90 @@
+// let word1 = 'hello';
+// console.log(word1);
+// alert(word1)
+// document.write(word1)
+//
+// let word2 = 'owu';
+// console.log(word2);
+// alert(word2);
+// document.write(word2);
+
+// let word3 = 'com';
+// console.log(word3);
+// alert(word3);
+// document.write(word3);
+
+// let word4 = 'ua';
+// console.log(word4);
+// alert(word4);
+// document.write(word4);
+
+// let n1 = 1;
+// console.log(n1);
+// alert(n1);
+// document.write(n1);
+
+// let n10 = 10;
+// console.log(n10);
+// alert(n10);
+// document.write(n10);
+
+// let n9 = -999;
+// console.log(n9);
+// console.log(typeof n9);
+// alert(n9);
+// document.write(n9);
+
+// let n123 = 123;
+// console.log(n123);
+// alert(n123);
+// document.write(n123);
+
+// let n314 = 3.14;
+// console.log(n314);
+// alert(n314);
+// document.write(n314);
+
+// let n27 = 2.7;
+// console.log(n27);
+// alert(n27);
+// document.write(n27);
+
+// let n16 = 16;
+// console.log(n16);
+// alert(n16);
+// document.write(n16);
+
+// let b = true;
+// console.log(b);
+// alert(b);
+// document.write(b);
+
+// let f = false;
+// console.log(f);
+// alert(f);
+// document.write(f);
+
+// let firstName = 'Tetiana';
+// let middleName = 'Tarasivna';
+// let lastName = 'Stetsko';
+// let person1 = firstName + ' ' + middleName + ' ' + lastName;
+// console.log(person1)
+//
+// let person2 = `${firstName} ${middleName} ${lastName}`;
+// console.log(person2)
+
+// let a = 100;
+// console.log(typeof a);
+//
+// let b = '100';
+// console.log(typeof b);
+//
+// let c = true;
+// console.log(typeof c);
+
+// let name = prompt('Name');
+// let middleName = prompt('Middle Name');
+// let age= prompt('Age');
+// console.log(name);
+// console.log(middleName);
+// console.log(age);
